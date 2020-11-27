19fd60e6a5767afbc1532eebeacb229e321552a8
Alec Wilson <alecwilson98@gmail.com>
2020-11-27 12:33:00 +0000

    Defined structure of document and initialised weights

--- model.py
+++ model.py
@@ -18,11 +18,48 @@ test = df_norm.iloc[400:]
 # Defines the main linear regression class
 class LinearRegression:
 
+    def __init__(self, iterations, learning_rate, mini_batch_size, X, Y):
+        # Hyper-parameters
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+        self.mini_batch_size = mini_batch_size
+
+        # Number of training examples and features
+        self.m, self.n = X.shape
+
+        # Initialising weights
+        self.W = np.zeros(self.n)
+        self.b = 0
+        self.X = X
+        self.Y = Y
+
+    def fit(self):
+        for i in range(self.iterations):
+            self.grad_des()
+
+        for i in range(self.iterations):
+            self.stochastic()
+
+        for i in range(self.iterations):
+            self.mini_batch()
+
+    def grad_des(self):
+
+    def stochastic(self):
+
+    def mini_batch(self):
+
+    def predict(self):
+
+    def visualisation(self):
+
 
 
 def main():
     lr = LinearRegression()
-
+    lr.fit()
+    lr.predict()
+    lr.visualisation()
 
 if __name__ == '__main__':
     main()
